91998c8a1830bcc4d95de2414ae25c2c030d0cfa
2018305 <krsihy@naver.com>
2024-05-10 16:22:46 +0900

    10주차

--- 10주차 수업.py
+++ 10주차 수업.py
@@ -1,14 +1,14 @@
+import random
 def ran_lotto():
     results = []
 
-import random
+    while len(results) < 6:
+        ran_num = random.randint(1, 45)
+        if ran_num in results:
+            print("results 안에 ran_num이 있으므로, results를 제거하고 출력했습니다.")
+        else:
+            results.append(ran_num)
 
-results =[]
-while len(results) < 6:
-    ran_num = random.randint(1,45)
-    if ran_num in results:
-        print("results 안에 ran_num이 있으므로, results에 추가하지 않습니다")
-    else:
-        results.append(ran_num)
+    return results
 
-print(results)
+print(ran_lotto())
